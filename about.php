83e54aa329f43a4189e1d53635527a332bb143f0
Steven <56438848+StvnRD@users.noreply.github.com>
2021-06-18 12:18:32 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title; ?></title>
+</head>
+<body>
+    <?= $this->include('template/header'); ?>
+    <div class="about-section">
+    </br>
+        <h1><center>Pemrograman Web - Pertemuan 12</center></h1>
+    </br> </br> </br> </br>
+        <center><img src="logo.png" height="250" width="300" align="middle">></center>
+    </br> </br> </br> </br>
+    </div>
+
+    <p3><center> Steven Ryan Darmawan</br></br> Kelas TI.19.C.1</br></br> NIM 311910524 </center></p3>
+    
+    <?= $this->include('template/footer'); ?>
+</body>
+</html>
